b3a620442468c0d3b2c3028500907182a24467f2
zpikachu <muzaif635@gmail.com>
2024-10-20 03:54:37 +0530

    2.4

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow connections from any IP
+    port: 5173, // Change to the port you want to use
+  },
 })
